ef42f6dfb2644548a84b19e1a601f5fe12ed9197
Ismail <ismail.desouki@gmail.com>
2021-04-13 09:59:44 -0700

    server

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+
+const routesReport = require('rowdy-logger').begin(app)
+const replaceInFile = require('replace-in-file')
+
+const path = require('path')
+const { kMaxLength } = require('buffer')
+
+app.get('/', (req, res) => {
+  const filepath = path.join(__dirname, 'index.html')
+  res.sendFile(filepath)
+})
+
+app.get('/main.js', async (req, res) => {
+  const filepath = path.join(__dirname, 'main.js')
+
+  if (process.env.NODE_ENV === 'production') {
+    await replaceInFile({
+      files: filepath,
+      from: 'http://localhost:3001/',
+      //to: heroku
+    })
+
+  }
+
+  res.sendFile(filepath)
+})
+
+app.get('/style.css', (req, res) => {
+  const filepath = path.join(__dirname, 'style.css')
+  res.type('css').sendFile(filepath)
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    routesReport.print()
+})
+//test
+
